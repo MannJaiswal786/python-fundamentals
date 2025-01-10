e7070341bfdaa869ee039267bf88fa93c8a11d5a
Mann Jaiswal <mannjaiswal5@gmail.com>
2025-01-09 18:37:02 -0700

    Practice variables, math operators and print statement in python

---
+++ cc1.py
@@ -0,0 +1,23 @@
+ #Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 1.2
+print(type(b))
+
+c = "Mann"
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
+
+
+
+
+
---
+++ logical_operators.py
@@ -0,0 +1,7 @@
+x = 100
+y = 200
+z = 100
+
+print(x < y and y > z)
+print(x > y or y > z)
+print(not(x > y or y > z))
