fe382c80540edcc9ae38ba29ff5af38c11cb698f
Mann Jaiswal <mannjaiswal5@gmail.com>
2025-01-13 17:42:34 -0700

    Played with python built in functions

---
+++ builtin_functions.py
@@ -0,0 +1,17 @@
+print("Math Functions")
+
+abs_int = abs(-1)
+print(abs_int)
+
+int_to_float = float(100)
+print(int_to_float)
+
+float_to_int = int(1.23)
+print(float_to_int)
+
+print(int(100.293))
+print(float(123))
+print(pow(2,4))
+print(min(1,2,-5,10,0))
+print(max(1,2,-5,10, 0))
+print(round(100.2304))
