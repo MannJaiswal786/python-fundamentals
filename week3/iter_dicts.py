79ea74fb02d43c5357b8b24a48368736d6bbf7a2
Mann Jaiswal <mannjaiswal5@gmail.com>
2025-02-26 17:40:24 -0700

    Worked with interating dictionaries in python

---
+++ week3/iter_dicts.py
@@ -0,0 +1,10 @@
+state_capitals = {"Washington": "Olympia", "Arizona": "Phoenix", "California": "Sacramento"}
+
+"""for state in state_capitals:
+    print(state)"""
+
+""" for city in state_capitals.values():
+    print(city) """
+
+for state in state_capitals:
+    print(state_capitals[state], "is the capital of ", state)
