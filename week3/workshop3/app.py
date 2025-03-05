763d6df58c7674e94fe621e0e9a1e8e41565a686
Mann Jaiswal <mannjaiswal5@gmail.com>
2025-03-05 11:13:11 -0700

    Added input functionality to let users decide the options

--- week3/workshop3/app.py
+++ week3/workshop3/app.py
@@ -3,9 +3,31 @@ database = {"admin": "password123"}
 donations = []
 authorized_user = ""
 
-show_homepage()
+while True:
+ show_homepage()
 
-if authorized_user == "":
+ if authorized_user == "":
     print("You must be logged in to donate")
-else:
+ else:
     print(f"Logged in as: {authorized_user}")
+
+ try:
+  option = int(input("Choose an option: "))
+ except ValueError:
+    print("Invaliid input. Please enter a number between 1 and 5.")
+    continue
+
+ if option == 1:
+    print("TODO: Write Login Functionality")
+ elif option == 2:
+    print("TODO: Write Register Functionality")
+ elif option == 3:
+    print("TODO: Write Donate Functionality")
+ elif option == 4:
+    print("TODO: Show Donations")
+ elif option == 5:
+    print("Exiting the program. Goodbye!!!")
+    break
+ else:
+    print("Invalid option. Please choose an option between 1 and 5")
+
