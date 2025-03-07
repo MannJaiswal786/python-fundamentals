641b94e7ba4fc92de61bf70eba5c0031a84e97dd
Mann Jaiswal <mannjaiswal5@gmail.com>
2025-03-07 09:46:48 -0700

    Updated workshop3 donations package and using_classes1.py, added new files

--- week4/using_classes1.py
+++ week4/using_classes1.py
@@ -1,6 +1,21 @@
+# class Player:
+#     max_hp = 4000
+
+# player1 = Player()
+# print(player1.max_hp)
+
+# Player.max_hp = 5000
+# print(player1.max_hp)
+
 class Player:
-    max_hp = 4000
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        self.score = 0
 
-player1 = Player()
 
-print(player1.max_hp)
+player1 = Player("Mann", 1200)
+player2 = Player("Tanishq", 1800)
+
+print("Player1: ", player1.name, player1.hp)
+print("Player2: ", player2.name, player2.hp)
